7cebd0a1aeeb91315465d4a4a7fd7571cfa7252d
Alden Vallestero <alden.v@agtuary.com>
2024-02-05 20:08:20 +0800

    🔧 Add axios configs

---
+++ src/clients/axios.client.ts
@@ -0,0 +1,14 @@
+import axios, { Axios } from 'axios';
+import { APP_REFERENCES } from 'src/commons/constants/references.constant';
+
+class AxiosClient {
+  axios: Axios;
+  constructor() {
+    this.axios = axios.create({
+      baseURL: APP_REFERENCES.COUNTRIES_BASE_URL,
+      timeout: 10000,
+    });
+  }
+}
+
+export default new AxiosClient();
