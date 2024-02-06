451a9cfb85e2cfdf2b42d428a158988404674802
Alden Vallestero <alden.v@agtuary.com>
2024-02-06 15:08:53 +0800

    ♻️ Secure constants

--- src/commons/constants/references.constant.ts
+++ src/commons/constants/references.constant.ts
@@ -1,4 +1,4 @@
 export const APP_REFERENCES = {
-  COUNTRIES_BASE_URL: 'https://restcountries.com/v3.1',
-  COUNTRIES_ALL_PATH: '/all',
+  COUNTRIES_BASE_URL: Object.freeze('https://restcountries.com/v3.1'),
+  COUNTRIES_ALL_PATH: Object.freeze('/all'),
 };
