4582917539a925f04ef8fcf3930fe99082831a15
Alden Vallestero <alden.v@agtuary.com>
2024-02-06 09:40:44 +0800

    🔧 Add axios response interceptor

--- src/clients/axios.client.ts
+++ src/clients/axios.client.ts
@@ -1,5 +1,6 @@
 import axios, { Axios } from 'axios';
 import { APP_REFERENCES } from 'src/commons/constants/references.constant';
+import logger from 'src/utils/logger.util';
 
 class AxiosClient {
   axios: Axios;
@@ -8,6 +9,15 @@ class AxiosClient {
       baseURL: APP_REFERENCES.COUNTRIES_BASE_URL,
       timeout: 10000,
     });
+    this.axios.interceptors.response.use(
+      (config) => {
+        return config;
+      },
+      (error) => {
+        logger.error(error?.response?.data?.message);
+        return Promise.reject(error);
+      },
+    );
   }
 }
 
